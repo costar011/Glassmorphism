7af469d5357e690d3d9e1272b3872482f51c7dd3
costar011 <dnaer456@gmail.com>
2023-12-25 21:28:43 +0900

    js

--- index.js
+++ index.js
@@ -5,3 +5,41 @@ var d = new Date();
 var month = d.getMonth()+1;
 
 day.innerHTML = d.getUTCDay();
+
+switch(month) {
+    case 1:mon.innerHTML = "JAN" + d.getFullYear();
+    break;
+
+    case 2:mon.innerHTML = "FEB" + d.getFullYear();
+    break;
+
+    case 3:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 4:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 5:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 6:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 7:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 8:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 9:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 10:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 11:mon.innerHTML = "" + d.getFullYear();
+    break;
+
+    case 12:mon.innerHTML = "" + d.getFullYear();
+    break;
+}
