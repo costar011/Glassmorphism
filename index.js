ac8145561ec85f26f8dd66f5178806d1fc9f0221
costar011 <dnaer456@gmail.com>
2024-01-05 19:59:21 +0900

    js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const day = document.getElementById("day");
+const mon = document.getElementById("mon");
+
+var d = new Date();
+var month = d.getMonth() + 1;
+
+day.innerHTML= d.getUTCDate();
+
+switch(month) {
+    case 1:mon.innerHTML = "JAN" + d.getFullYear();
+    break;
+}
