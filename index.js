e07c4dd0b3684f33f68677bbbf2bf3ad3ffd9894
costar011 <dnaer456@gmail.com>
2024-01-06 21:30:55 +0900

    js

--- index.js
+++ index.js
@@ -9,4 +9,37 @@ day.innerHTML= d.getUTCDate();
 switch(month) {
     case 1:mon.innerHTML = "JAN" + d.getFullYear();
     break;
+
+    case 2:mon.innerHTML = "FEB" + d.getFullYear();
+    break;
+
+    case 3:mon.innerHTML = "MAR" + d.getFullYear();
+    break;
+
+    case 4:mon.innerHTML = "APR" + d.getFullYear();
+    break;
+
+    case 5:mon.innerHTML = "MAY" + d.getFullYear();
+    break;
+
+    case 6:mon.innerHTML = "JUN" + d.getFullYear();
+    break;
+
+    case 7:mon.innerHTML = "JUL" + d.getFullYear();
+    break;
+
+    case 8:mon.innerHTML = "AUG" + d.getFullYear();
+    break;
+
+    case 9:mon.innerHTML = "SEP" + d.getFullYear();
+    break;
+
+    case 10:mon.innerHTML = "OCT" + d.getFullYear();
+    break;
+
+    case 11:mon.innerHTML = "NOV" + d.getFullYear();
+    break;
+
+    case 12:mon.innerHTML = "DEC" + d.getFullYear();
+    break;
 }
